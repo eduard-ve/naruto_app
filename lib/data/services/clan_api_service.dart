a9c971f1a0884f7c46c2f8970826a4bf0fca1a16
Eduard <eduardvelez066@gmail.com>
2025-05-18 17:47:54 -0500

    ajustes en en forma de acceder al respuesta Json

--- lib/data/services/clan_api_service.dart
+++ lib/data/services/clan_api_service.dart
@@ -1,22 +1,32 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:naruto_app/data/models/clan.dart';
+import 'package:logger/logger.dart'; // Importa el paquete logger
 
 class ClanService {
   final String baseUrl = 'https://dattebayo-api.onrender.com/clans';
 
   Future<List<ClanModel>> fetchClans({int limit = 10, int offset = 0}) async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/clans?limit=$limit&offset=$offset'),
-    );
+    final url = Uri.parse('$baseUrl?limit=$limit&offset=$offset');
+    final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      final jsonBody = json.decode(response.body);
-      final List<dynamic> data =
-          jsonBody['data']; // Ajusta si la API responde con un objeto
-      return data.map((json) => ClanModel.fromJson(json)).toList();
+      try {
+        final jsonBody = json.decode(response.body);
+        final List<dynamic> data = jsonBody['clans'];
+        return data.map((json) => ClanModel.fromJson(json)).toList();
+      } catch (e) {
+        Logger().e('Error al decodificar JSON: $e',
+            error: e); // Usa Logger().e con argumento nombrado
+        throw Exception('Error al decodificar JSON: $e');
+      }
     } else {
-      throw Exception('Error al cargar los clanes');
+      Logger().e(
+          // Usa Logger().e con argumentos nombrados
+          'Error al cargar clanes: Status code: ${response.statusCode}, Body: ${response.body}',
+          error: response
+              .statusCode); // Puedes pasar el código de estado como error
+      throw Exception('Error al cargar clanes: ${response.statusCode}');
     }
   }
 }
